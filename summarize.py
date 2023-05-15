0957102c48b103efb6ced709f7bb904426ed7c13
Debarghya_Dey <64305801+DebarghyaDey@users.noreply.github.com>
2023-05-15 12:22:44 +0530

    Update summarize.py
    Added proper indentation.

--- summarize.py
+++ summarize.py
@@ -22,18 +22,24 @@ BUILDING_CATEGORIES_PLURAL = [' banks or ATMs', ' hospitals or medical shops', '
 
 def format_building_counts(building_counts: list[list[str]]):
     building_summary = ""
+    # printing first summary
     for i in range(3):
-        if not building_counts[i]:
-            continue
-        building_summary += f"Within a distance of {str(i + 1)} km, we have "
-        for j in range(len(building_counts[i])):
-            if len(building_counts[i]) == 1:
-                building_summary += f"{building_counts[i][j]}.\n\n"
-            elif j == len(building_counts[i]) - 1:
-                building_summary += f"and {building_counts[i][j]}.\n\n"
-            else:
-                building_summary += f"{building_counts[i][j]}, "
-
+      if output[i] == []: continue
+      if i==0:
+        building_summary += f"Within a distance of 1 km, we have "
+      elif i==1:
+        building_summary += f"Increasing the search space to 2 km yields in extra establishments such as "
+      else :
+        building_summary += f"Overall within 3 km, in addition to previously discovered buildings, one can come across "
+      for j in range(len(output[i])):
+        if len(output[i]) == 1:
+          building_summary += f"{building_counts[i][j]}.\n\n"
+        elif j == len(output[i])-1:
+          building_summary += f"{building_counts[i][j]}.\n\n"
+        elif j== len(output[i])-2:
+          building_summary += f"{building_counts[i][j]}.\n\n"
+        else:
+          building_summary += f"{building_counts[i][j]},"
     return building_summary
 
 
